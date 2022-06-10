9a6c2d42f101a37166ffc99fcbcdad3c5b9ad155
NazarenaAranda <nazarenaaranda15@gmail.com>
2022-06-10 10:34:01 -0700

    cambio de condicion

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -10,19 +10,19 @@ void bubble_sort(int *array, size_t size)
 {
 	int rec, rec2, aux;
 
-	for (rec = 0; rec < 10; rec++)
+	for (rec = 0; rec <= 9; rec++)
 	{
-		for (rec2 = 0; rec2 < 10; rec2++)
+		for (rec2 = 0; rec2 <= 9; rec2++)
 		{
 			if (array[rec2] > array[rec2 +1])
 			{
 				aux = array[rec2 + 1];
 				array[rec2 + 1] = array[rec2];
 				array[rec2] = aux;
+				print_array(array, size);
 			}
 		}
 	}
-	print_array(array, size);
 }
 
 
