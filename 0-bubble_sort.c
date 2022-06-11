aa95bfb5d77b31bea7aea99b77b507bea0cd7566
NazarenaAranda <nazarenaaranda15@gmail.com>
2022-06-11 00:01:38 -0700

    funcion nueva de intercambio

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,24 +1,40 @@
 #include "sort.h"
+/**
+ * _intercambio - intercambiar 2punteros(marcan posiciones)
+ * @uno: primer puntero
+ * @dos: segundo puntero
+ */
+
+void _intercambio(int *uno, int *dos)
+{
+	int temp;
+
+	temp = *uno;
+	*uno = *dos;
+	*dos = temp;
+}
 
 /**
  * bubble_sort - ordenar una array de integers
- * @array: donde estan los enteros
- * @size: tamaño
+ * @array: donde estan los enteros a imprimir
+ * @size: numero de elementos en la array
+ * Return: void
  */
 
 void bubble_sort(int *array, size_t size)
 {
-	int recorrido, cambios, aux;
+	long unsigned int recorrido, cambios;
 
-	for (recorrido = 0; recorrido <= 9; recorrido++)
+	if (size <= 1)
+		return;
+
+	for (recorrido = 0; recorrido < (size -1); recorrido++)
 	{
-		for (cambios = 0; cambios <= 9; cambios++)
+		for (cambios = 0; cambios < size -1; cambios++)
 		{
-			if (array[cambios] > array[cambios + 1])
+			if (array[cambios -1] > array[cambios])
 			{
-				aux = array[cambios + 1];
-				array[cambios + 1] = array[cambios];
-				array[cambios] = aux;
+				_intercambio(&array[cambios-1], &array[cambios]);
 				print_array(array, size);
 			}
 		}
